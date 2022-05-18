3f230447b2f59c1c14690bf8af3da8d65ac99713
Eva Reitzel <evareitzel@gmail.com>
2022-05-17 22:11:37 -0400

    commit tree 28e519d29b066b6f65e2f472e6b3d02db7e77060

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+const currentUser = 'Vincent Thompson';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+// .slice() method is more flexible
+
+// const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+// const shortGreeting = `Welcome, ${currentUser[0]}!`;
